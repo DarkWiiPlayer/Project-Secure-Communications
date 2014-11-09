29f5d13859aaca66485216a9478bee56270008fa
DarkWiiPlayer <DarkWiiPlayer@hotmail.com>
2014-11-09 16:43:09 +0100

    Implemented shiftRows function
    This function basically just interchanges some fields of the input block

--- shiftRows.cpp
+++ shiftRows.cpp
@@ -1,5 +1,10 @@
 
-void shiftRows(char *block)
+void shiftRows(uint32_t *block)
 {
+    uint32_t hlp;
 
+    hlp = block[4]; block[4] = block[5]; block[5] = block[6]; block[6] = block[7]; block[7] = hlp;
+    hlp = block[8]; block[8] = block[10]; block[10] = hlp;
+    hlp = block[9]; block[9] = block[11]; block[11] = hlp;
+    hlp = block[15]; block[15] = block[14]; block[14] = block[13]; block[13] = block[12]; block[12] = hlp;
 }
